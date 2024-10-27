cc11c6346d4c04fbea4af85af2dd3a4174b9faaf
OttoElomaa <otto.elomaa99@gmail.com>
2024-10-27 19:54:18 +0200

    ReactRouter Project: Created new project for an exercise

---
+++ ReactRouterExercise/react-router/src/main.jsx
@@ -0,0 +1,43 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import './index.css'
+import App from './App.jsx'
+
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
+import Error from './Error.jsx';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+
+    children: [                       // children are nested routes with a route
+      {
+        element: <Home />,
+        index: true                   // index route does not need any path
+      },
+      {
+        path: "about",                // path can be defined relative to the parent path
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
